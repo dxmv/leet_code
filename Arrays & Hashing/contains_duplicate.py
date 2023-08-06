0fa718b59294e4f535d6cbafb08f65108d66b06c
dxmv <dimitirjestepanovic1@gmail.com>
2023-08-06 15:13:32 +0200

    217. Contains Duplicate

---
+++ Arrays & Hashing/contains_duplicate.py
@@ -0,0 +1,12 @@
+# 217. Contains Duplicate
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        map={}
+        for n in nums:
+            if n in map:
+                return True
+            else:
+                map[n]=True
+        return False
