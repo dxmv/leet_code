659f5db7242256a8e98d7769fb252f4e3903dbd0
dxmv <dimitirjestepanovic1@gmail.com>
2023-08-08 11:10:36 +0200

    206. Reverse Linked List

---
+++ Linked List/reverse.py
@@ -0,0 +1,15 @@
+# 206. Reverse Linked List
+class Solution:
+    # Time: O(logn), Memory: O(1)
+    def search(self, nums: List[int], target: int) -> int:
+        l=0
+        r=len(nums)-1
+        while l<=r:
+            mid=(r+l)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                r=mid-1
+            else:
+                l=mid+1
+        return -1
