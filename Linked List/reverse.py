06ffd80374094d10ab8e8258910d5b65f4f6f633
dxmv <dimitirjestepanovic1@gmail.com>
2023-08-08 12:34:22 +0200

    206. Reverse Linked List Iteratively

--- Linked List/reverse.py
+++ Linked List/reverse.py
@@ -1,1 +1,16 @@
 # 206. Reverse Linked List
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    # Time: O(n), Memory: O(n)
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current=None
+        while head:
+            current=ListNode(val=head.val,next=current)
+            head=head.next
+        return current
+    
+    
