dcc0ed41873cb233ed4c48b6d96d7060dcbcc18b
dxmv <dimitirjestepanovic1@gmail.com>
2023-08-09 12:12:21 +0200

    121. Best Time to Buy and Sell Stock

---
+++ Sliding Window/best_time_to_buy_sell_stock.py
@@ -0,0 +1,15 @@
+# 121. Best Time to Buy and Sell Stock
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l,r=0,1
+        max=0
+        while r<len(prices):
+            if prices[r]-prices[l]>max:
+                max=prices[r]-prices[l]
+            if prices[l]>=prices[r]:
+                l=r
+                r+=1
+            else:
+                r+=1
+        return max
