d7dfd8d54ecb73a3aa3d047ee58de288f3c438e9
dxmv <dimitirjestepanovic1@gmail.com>
2023-09-14 10:06:57 +0200

    487 Max Consecutive Ones II

--- Sliding Window/max_consecutive_ones.py
+++ Sliding Window/max_consecutive_ones.py
@@ -1,3 +1,4 @@
+# These are not in the list
 # 485. Max Consecutive Ones
 
 class Solution:
@@ -15,5 +16,30 @@ class Solution:
                 l=r
             r+=1
         return max(num_ones,current)
+
+# 487. Max Consecutive Ones II
+class Solution:
+
+    def find_max_consecutive_ones(self, nums: List[int]) -> int:
+        l,r=0,0
+        current=0
+        res=0
+        index_zero=-1
+        while r<=len(nums)-1:
+            c=nums[r]
+            if c==1:
+                current+=1
+            else:
+                if index_zero==l:
+                    l+=1
+                    current-=1
+                if index_zero!=-1:
+                    nums[index_zero]=0
+                index_zero=r
+                nums[index_zero]=1
+                current+=1
+            res=max(current,res)
+            r+=1
+        return res
 
 # 1004. Max Consecutive Ones III
